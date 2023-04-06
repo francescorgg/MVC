7fd4863deb6c69d843c7e522926e816a807ebc37
francescorgg <frn.rgg@gmail.com>
2023-04-06 17:40:25 +0200

    PDO test

--- index.php
+++ index.php
@@ -1,8 +1,21 @@
 <?php
 
 require "functions.php";
-require "router.php";
+//require "router.php";
 
 
 
-routeToController($uri, $routes);
+//routeToController($uri, $routes);
+
+$dsn = "mysql:host=localhost;port=3306;dbname=sakila;charset=utf8mb4";
+
+
+$pdo = new PDO($dsn, 'root');
+
+$statement = $pdo->prepare("SELECT * FROM articoli");
+
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+dd($posts);
