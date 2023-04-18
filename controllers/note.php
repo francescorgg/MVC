c86214aa0720d5e79cec2d19fbc70a805b1fab10
francescorgg <frn.rgg@gmail.com>
2023-04-18 22:34:40 +0200

    add authorization feature

--- controllers/note.php
+++ controllers/note.php
@@ -5,7 +5,10 @@ $config = (require "conf.php");
 $db = new Database($config['database']);
 
 
-$notes = $db->query('SELECT * FROM articoli WHERE articleid = :id', ['id' => $_GET['articleid']])->fetchAll();
+$notes = $db->query('SELECT * FROM articoli WHERE id_utenti = :id_utente AND articleid = :id', ['id_utente' => 1, 'id' => $_GET['articleid']])->fetchAll();
 
+if(!$notes){
+    abort();
+}
 
 require "view/notes.view.php";
