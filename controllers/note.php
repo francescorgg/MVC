c66669ea518d508388be034f5bca44d0eebd5faf
francescorgg <frn.rgg@gmail.com>
2023-04-18 23:17:40 +0200

    new class Response

--- controllers/note.php
+++ controllers/note.php
@@ -4,11 +4,16 @@ $title = 'Note';
 $config = (require "conf.php");
 $db = new Database($config['database']);
 
+$user = 1;
 
-$notes = $db->query('SELECT * FROM articoli WHERE id_utenti = :id_utente AND articleid = :id', ['id_utente' => 1, 'id' => $_GET['articleid']])->fetchAll();
+$notes = $db->query('SELECT * FROM articoli WHERE articleid = :id', ['id' => $_GET['articleid']])->fetchAll();
 
 if(!$notes){
     abort();
+}
+
+if($notes[0]['id_utenti'] != $user){
+    abort(Response::FORBIDDEN);
 }
 
 require "view/notes.view.php";
---
+++ view/403.php
@@ -0,0 +1,15 @@
+<?php require "partials/head.php"; ?>
+
+<div class="min-h-full">
+
+    <?php require "partials/nav.php"; ?>
+
+    <main>
+        <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+            <h2>Non sei autorizzato a visualizzare questa risorsa</h2>
+            <li class="mt-5 hover:underline hover:text-blue-600"><a href="/" aria-current="page">Ritorna alla Homepage</a></li>
+        </div>
+    </main>
+</div>
+
+<?php require "partials/footer.php"; ?>
