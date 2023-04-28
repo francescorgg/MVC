8c525bcc747a360d4904cad99640f10167b1d6a2
francescorgg <frn.rgg@gmail.com>
2023-04-28 16:00:45 +0200

    rewrite routes logic

--- router.php
+++ router.php
@@ -2,12 +2,7 @@
 
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
-$routes = [
-    '/' => 'controllers/index.php',
-    '/about' => 'controllers/about.php',
-    '/notes' => 'controllers/notes.php',
-    '/note' => 'controllers/note.php'
-];
+$routes = require 'routes.php';
 
 routeToController($uri, $routes);
 
---
+++ routes.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    '/' => 'controllers/index.php',
+    '/about' => 'controllers/about.php',
+    '/notes' => 'controllers/notes.php',
+    '/note' => 'controllers/note.php'
+];
